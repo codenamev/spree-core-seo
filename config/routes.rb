ebc0f6d4ffd03d3b4f7412593228a0c8a11ed5b5
Valentino Stoll <valentino@mosaicwebsite.com>
2011-09-12 13:49:55 -0400

    Reverting routes to debug admin portion for 0-60-x

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   #match '/product_list.:format' => "sitemap#product_list"
 
   namespace :admin do
-    #resource :keywords_footer
-    #resource :sitewide_seo
+    resource :keywords_footer
+    resource :sitewide_seo
   end
 end
